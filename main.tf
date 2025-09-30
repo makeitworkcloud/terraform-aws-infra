ef8cced5fd6fbe2221da79d7bcc709ce0933cfd7
xnoto <steven@makeitwork.cloud>
2025-09-29 22:31:05 -0600

    Adding libvirt bucket

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ locals {
   s3_private_buckets = toset([
     "mitw-tf-aws-infra",
     "mitw-tf-cloudflare-infra",
-    "mitw-tf-github-repos"
+    "mitw-tf-github-repos",
+    "mitw-tf-libvirt-infra"
   ])
   s3_public_buckets = toset([])
   s3_web_buckets = toset([
